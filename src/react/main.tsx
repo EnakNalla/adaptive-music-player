ddc7bf2067f6ada6009359ac29b45f223e2d949b
EnakNalla <enak.nalla@gmail.com>
2022-05-21 22:04:44 +0100

    fix: add react-toastify css

--- src/react/main.tsx
+++ src/react/main.tsx
@@ -5,6 +5,7 @@ import '@fontsource/roboto/700.css';
 import StoreProvider from '@stores';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'react-toastify/dist/ReactToastify.min.css';
 import App from './App/App';
 import './styles.css';
 
